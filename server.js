18c640486176cbd60efbe916e9a3c055349bfc56
Kirill Stepanov <k.v.stepanov@icloud.com>
2020-09-22 21:21:09 +0200

    Change the message to test redeployment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello from the other side!');
 });
 
 app.listen(PORT, HOST);
